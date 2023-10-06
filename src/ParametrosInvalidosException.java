224b98ba065c3f0ef96cee0ccf6b6e07a481cefe
RafaelFerreira18 <rafabf18@gmail.com>
2023-10-06 13:16:30 -0300

    Finished project

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception{
+    public String parametrosInvalidos() throws Exception{
+        return "Paramêtros inválidos, parâmetro um maior que parâmetro 2";
+    }
+}
